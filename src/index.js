1ae8df4d49065c189bc10493cf7eb40a5025987f
Pavel <Chesh397@mail.ru>
2019-06-28 18:16:42 +0300

    Create dev configuration

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './js/main.js';
+import './css/style.css';
+import './css/super.css';
---
+++ webpack.config.dev.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    port: 8787,
+    writeToDisk: true,
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', 'link:href'],
+          }
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        }],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:8787'
+    }),
+  ],
+};
